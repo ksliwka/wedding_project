e7d671de0e7fdfdc9dfa669ad51b828cecea4bcd
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-07-26 17:37:29 +0200

    adding loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import classes from "./loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={classes.loading_container}>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Login = () => {
+  return (
+    <div>
+      <p>Login</p>
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+};
+
+export default Login;
